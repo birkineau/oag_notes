1e4e67b76eaffb8bb6c6c7195ff09df86b378f06
Orlys Azaharez Garcia <orlys.azaharez@lacoloniamedical.com>
2024-04-23 18:10:37 -0400

    remove firebase_options

--- notes_flutter/lib/main.dart
+++ notes_flutter/lib/main.dart
@@ -1,19 +1,14 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_client/notes_client.dart';
-import 'package:notes_flutter/firebase_options.dart';
 import 'package:notes_flutter/notes_flutter.dart';
 import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
 
 /// TODO: ensure connected to server
 /// TODO: add biometrics
-/// TODO: add loading indicators for async operations such as sign-in, sign-out
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   di
     ..registerSingleton(
